84585122acdbe7ccffbb753bc5c746c31d242336
Kasun Sagara <kasunsagara689@gmail.com>
2026-01-01 23:37:52 +0530

    create user.js model file

---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["patient", "doctor", "admin"],
+        default: "patient"
+    }
+})
+
+const User = mongoose.model("users", userSchema)
+
+export default User;
